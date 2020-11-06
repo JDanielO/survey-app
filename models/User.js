5b44b819239aed842df8b238f7db636fe5e496a7
Daniel Oropeza <jdanielosv@hotmail.com>
2020-11-06 09:44:53 -0500

    fix typos

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const moongoose = require("mongoose");
-const { Schema } = moongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
