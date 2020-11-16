81d528facc3e3e6257be3b741488113bbc36a5b2
Daniel Oropeza <jdanielosv@hotmail.com>
2020-11-16 11:21:37 -0500

    work on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <div class="ui menu">
+        <div class="header item">Our Company</div>
+        <a class="item">About Us</a>
+        <a class="item">Jobs</a>
+        <a class="item">Locations</a>
+      </div>
+    );
   }
 }
 
