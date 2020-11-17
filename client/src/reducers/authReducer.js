5a0795eef83088a6966749766532a3b5b014d70b
Daniel Oropeza <jdanielosv@hotmail.com>
2020-11-17 10:48:52 -0500

    auth_reducer return values

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-export default function (state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from "../action/types";
+
+export default function (state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false; // the user model (if logged in), if not it returns "". Remember: ('' || false) === false
     default:
       return state;
   }
