5210c4ef4830785196ddeaddd835131f8eaa7775
Daniel Oropeza <jdanielosv@hotmail.com>
2020-11-23 13:44:20 -0500

    add surveys, beginning

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,17 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
-const requireLogin = require("../middlewares/requireCredits");
 const requireCredits = require("../middlewares/requireCredits");
 
+const Survey = mongoose.model("surveys");
+
 module.exports = (app) => {
-  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {});
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+    });
+  });
 };
