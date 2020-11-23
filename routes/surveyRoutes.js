37f4461386cffc49505af6d67c6630b5bbe139ee
Daniel Oropeza <jdanielosv@hotmail.com>
2020-11-23 13:34:08 -0500

    verifying minimum credits

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,7 @@
 const requireLogin = require("../middlewares/requireLogin");
+const requireLogin = require("../middlewares/requireCredits");
+const requireCredits = require("../middlewares/requireCredits");
 
 module.exports = (app) => {
-  app.post("api/surveys", requireLogin, (req, res) => {});
+  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {});
 };
