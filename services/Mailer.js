f901291639602fe09b96770be1d9071a1a4c38fc
Daniel Oropeza <jdanielosv@hotmail.com>
2020-12-22 14:54:01 -0800

    finishSurveyCreation_axioswont send email yet

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("kaibatestcorp@gmail.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -23,7 +24,7 @@ class Mailer extends helper.Mail {
   }
 
   addClickTracking() {
-    const trackingSettings = new helper.trackingSettings();
+    const trackingSettings = new helper.TrackingSettings();
     const clickTracking = new helper.ClickTracking(true, true);
 
     trackingSettings.setClickTracking(clickTracking);
@@ -32,10 +33,22 @@ class Mailer extends helper.Mail {
 
   addRecipients() {
     const personalize = new helper.Personalization();
+
     this.recipients.forEach((recipient) => {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON(),
+    });
+
+    const response = await this.sgApi.API(request);
+    return response;
   }
 }
 
