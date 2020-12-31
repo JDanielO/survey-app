f3160bb6e372d5a72d7f4f11fe10433f5c39935a
Daniel Oropeza <jdanielosv@hotmail.com>
2020-12-31 15:16:16 -0800

    style the form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
+
 import Header from "./Header";
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
@@ -11,6 +12,7 @@ class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
+
   render() {
     return (
       <div className="container">
